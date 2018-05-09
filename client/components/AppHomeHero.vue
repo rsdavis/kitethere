8f220b5ac62ec34a4697d82519c1c1ccc8cf03ec
Ryan Davis <rsdavis@Ryans-MacBook-Pro.local>
2018-05-09 13:09:58 -0500

    changes

---
+++ client/components/AppHomeHero.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hero">
+    <div class="hero__content">
+      <div class="hero__message">hello</div>
+      <img class="hero__swoosh" src="@/assets/content-swoosh.svg">
+      <img class="hero__img" src="@/assets/cropped.jpg" alt="">
+    </div>
+  </div>
+</template>
+
+<style>
+.hero {
+}
+
+.hero__content {
+  position: relative;
+
+  display: grid;
+  grid-template-columns: var(--layout);
+  grid-template-rows: auto;
+}
+
+.hero__message {
+  grid-row: 1;
+  grid-column: 2;
+  height: 100%;
+  width: 100%;
+}
+
+.hero__swoosh {
+  grid-row: 1;
+  grid-column: 2;
+
+  position: absolute;
+  left: 20%;
+  z-index: 2;
+  width: 31%;
+  transform: scaleY(-1);
+}
+
+.hero__img {
+  grid-row: 1;
+  grid-column: 2;
+
+  position: absolute;
+  left: 20%;
+  width: 80%;
+}
+</style>
---
+++ client/components/AppNavbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="nav">
+    <div class="nav__content">
+      <div class="nav__logo">KiteThere</div>
+      <div class="nav__link">about</div>
+      <div class="nav__link">map</div>
+    </div>
+  </div>
+</template>
+
+<style>
+.nav {
+  border-bottom: 1px solid rgb(220, 220, 220);
+  height: 80px;
+  display: grid;
+  grid-template-columns: var(--layout);
+}
+
+.nav__content {
+  grid-column: 2;
+  display: grid;
+  grid-template-columns: 1fr 100px 100px;
+  align-items: center;
+}
+
+.nav__logo {
+  font-size: 32px;
+  font-weight: bold;
+  color: rgb(30, 40, 50);
+}
+
+.nav__link {
+  justify-self: right;
+}
+</style>
